8f9de6d8aa2412e8ec7bdd54ac90387f2d226467
Ali Behrooz <ali.behrooz.2002@gmail.com>
2024-02-19 16:10:47 +0330

    change some line

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'welcome to my bot. \n Type /help for a list of commands.')
+    bot.reply_to(message, """welcome to my bot. 
+                                    \n Type /help for a list of commands.""")
 
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "how can I help you? \n /hello for say hi \n /start for start \n now i can't do anything but we update it soon ")
+    bot.reply_to(message, """how can I help you? 
+                                    \n /start for start 
+                                    \n /hello for say hi 
+                                    \n now i can't do anything but we update it soon """)
 
 
 @bot.message_handler(commands=['hello'])
